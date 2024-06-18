4684dddffa883f27e1980c19a7c217e8f5ffb2d4
eliazerhernandez <hernandezeliazer65@gmail.com>
2024-06-17 18:14:05 -0600

    Proyecto para empresa Cemza

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Función de reducción de la barra de navegación
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Reducir barra de navegación 
+    navbarShrink();
+
+    // Reducir la barra de navegación cuando se desplaza la página
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Active bootstrap scrollspy en el elemento de navegación principal
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Contraer la barra de navegación receptiva cuando el conmutador esté visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+
+});
